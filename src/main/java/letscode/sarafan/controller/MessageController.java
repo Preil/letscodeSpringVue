c62702e06ad272f12f0cab989e95a1cf2b399e8b
preil <preil2000@gmail.com>
2018-07-02 18:18:58 +0300

    Rest controller created, implementation started.

---
+++ src/main/java/letscode/sarafan/controller/MessageController.java
@@ -0,0 +1,26 @@
+package letscode.sarafan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    public List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>(){{put("id", "1"); put("text", "First message");}});
+        add(new HashMap<String, String>(){{put("id", "2"); put("text", "Second message");}});
+        add(new HashMap<String, String>(){{put("id", "3"); put("text", "Third message");}});
+        add(new HashMap<String, String>(){{put("id", "4"); put("text", "Forth message");}});
+    }};
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
+
+}
