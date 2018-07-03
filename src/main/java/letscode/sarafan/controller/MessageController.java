e1463aa93cd773ca559aa81c0554a45abd830d85
preil <preil2000@gmail.com>
2018-07-03 14:45:41 +0300

    Single message mapping added. NotFoundException added.

--- src/main/java/letscode/sarafan/controller/MessageController.java
+++ src/main/java/letscode/sarafan/controller/MessageController.java
@@ -1,6 +1,8 @@
 package letscode.sarafan.controller;
 
+import letscode.sarafan.exceptions.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,24 @@ import java.util.Map;
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    public List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
         add(new HashMap<String, String>(){{put("id", "1"); put("text", "First message");}});
         add(new HashMap<String, String>(){{put("id", "2"); put("text", "Second message");}});
         add(new HashMap<String, String>(){{put("id", "3"); put("text", "Third message");}});
         add(new HashMap<String, String>(){{put("id", "4"); put("text", "Forth message");}});
     }};
+
     @GetMapping
     public List<Map<String, String>> list() {
         return messages;
+    }
+
+    @GetMapping ("{id}")
+    public Map<String, String> getOne (@PathVariable String id){
+        return messages.stream()
+                .filter(message -> message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
     }
 
 }
