c3ef8b903ccbc9f68f20e15e32881e0cc948033a
preil <preil2000@gmail.com>
2018-07-20 16:26:24 +0300

    Refactored some dependencies and created Message entity.

--- src/main/java/letscode/sarafan/SarafanApplication.java
+++ src/main/java/letscode/sarafan/SarafanApplication.java
@@ -1,12 +1,16 @@
 package letscode.sarafan;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SarafanApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SarafanApplication.class, args);
 	}
+
 }
---
+++ src/main/java/letscode/sarafan/domain/Message.java
@@ -0,0 +1,59 @@
+package letscode.sarafan.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Message {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Message)) return false;
+
+        Message message = (Message) o;
+
+        return id.equals(message.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
