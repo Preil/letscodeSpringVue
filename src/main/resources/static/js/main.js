c0c36f2f1b7e6e6198d76abfcbe41f722455e745
preil <preil2000@gmail.com>
2018-07-20 13:36:08 +0300

    Vue-resource added. Getting messages from server via vue-resource instance. Created - make request to server when component is nearly created and populate messages with massages from request.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,25 +1,28 @@
+var messageApi = Vue.resource('/message{/id}')
 
-Vue.component('message-row',{
+Vue.component('message-row', {
     props: ['message'],
     template: '<div><i>({{message.id}})</i>{{message.text}}</div>'
 })
 
 Vue.component('messages-list', {
-    props:['messages'],
-   template: '<div>' +
-   '<message-row v-for="message in messages" :key="message.id" :message="message"></message-row>' +
-   '</div>'
+    props: ['messages'],
+    template: '<div>' +
+    '<message-row v-for="message in messages" :key="message.id" :message="message"></message-row>' +
+    '</div>',
+    created: function () {
+        messageApi.get().then(result =>
+            result.json().then(data =>
+                data.forEach(message => this.messages.push(message))
+            )
+        )
+    }
 });
 
 var app = new Vue({
     el: '#app',
     template: '<messages-list :messages="messages"></messages-list>',
     data: {
-        messages:
-            [
-            {id: '123', text: "wow"},
-            {id: '12', text: "more"},
-            {id: '1', text: "another"}
-            ]
+        messages: []
     }
 });
