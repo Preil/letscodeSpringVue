7a053eff5ab2bc2c79dd6fb0fdd793a837e71069
preil <preil2000@gmail.com>
2018-07-20 12:53:25 +0300

    Working with vue.component and props and templates.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,20 @@
+
+Vue.component('messages-list', {
+    props:['messages'],
+   template: '<div>' +
+   '<div v-for="message in messages">{{message.text}}</div>' +
+   '</div>'
+});
+
 var app = new Vue({
     el: '#app',
+    template: '<messages-list :messages="messages"></messages-list>',
     data: {
-        message: 'Hello Vue!'
+        messages:
+            [
+            {id: '123', text: "wow"},
+            {id: '12', text: "more"},
+            {id: '1', text: "another"}
+            ]
     }
-})
+});
